b554a6308e93b5318534750365f5c68149c26eb2
Josh Meekhof <joshua_meekhof@optum.com>
2019-02-04 22:56:17 -0500

    Add gsqlCopySources task to project

--- src/main/kotlin/com/optum/gradle/tigergraph/GsqlPlugin.kt
+++ src/main/kotlin/com/optum/gradle/tigergraph/GsqlPlugin.kt
@@ -1,13 +1,20 @@
 package com.optum.gradle.tigergraph
 
-import org.gradle.api.Project
 import org.gradle.api.Plugin
+import org.gradle.api.Project
 
 open class GsqlPlugin : Plugin<Project> {
 
     override fun apply(project: Project): Unit = project.run {
         // Register extension for dsl
         extensions.create("tigergraph", GsqlPluginExtension::class.java)
+
+        // Create CopySources task
+        project.tasks.run {
+            create("gsqlCopySources", GsqlCopySources::class.java) {
+                it.group = "Development"
+            }
+        }
 
         with(project) {
             logger.lifecycle("GSQL Plugin successfully applied to {$project.name}")
