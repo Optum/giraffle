21fc1a9583758cd5a209e6fad6456e9e9ae9f9d8
Josh Meekhof <joshua_meekhof@optum.com>
2019-02-04 22:29:36 -0500

    Add custom CopySource task

---
+++ src/main/kotlin/com/optum/gradle/tigergraph/GsqlCopySources.kt
@@ -0,0 +1,42 @@
+package com.optum.gradle.tigergraph
+
+import org.apache.tools.ant.filters.ReplaceTokens
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.InputDirectory
+import org.gradle.api.tasks.OutputDirectory
+import org.gradle.api.tasks.TaskAction
+import java.io.File
+
+open class GsqlCopySources : DefaultTask() {
+    private val extension: GsqlPluginExtension = project.extensions.findByName("tigergraph") as GsqlPluginExtension
+
+    @InputDirectory
+    val inputDir: File = project.file("${project.rootDir}/${extension.scriptPath}")
+
+    @Input
+    val tokens: Map<String, String> = extension.tokens
+
+    @OutputDirectory
+    val outputDir: File = project.file("${project.buildDir}/${extension.scriptPath}")
+
+    init {
+        group = "Development Tasks"
+        description = "Copy gsql script from scriptPath to build directory."
+    }
+
+    @TaskAction
+    fun copyFiles() {
+        inputDir.apply { parentFile.mkdirs() }
+
+        project.copy {
+            it.run {
+                from(inputDir) {
+                    include("**/*.gsql")
+                    filter(mapOf("tokens" to tokens), ReplaceTokens::class.java)
+                }
+                into(outputDir)
+            }
+        }
+    }
+}
