86f63265a82b1bd3030065ffa90be75c6b79cb9a
Josh Meekhof <joshua_meekhof@optum.com>
2019-02-08 21:44:46 -0500

    Add name for gsql shell task

--- src/main/kotlin/com/optum/gradle/tigergraph/GsqlPlugin.kt
+++ src/main/kotlin/com/optum/gradle/tigergraph/GsqlPlugin.kt
@@ -7,13 +7,23 @@ open class GsqlPlugin : Plugin<Project> {
     /**
      * The name of the extension for configuring the runtime behavior of the plugin.
      *
+     * @see com.optum.gradle.tigergraph.GsqlPluginExtension
      */
     val EXTENSION_NAME = "tigergraph"
 
     /**
      * The name of the task that copies the GSQL source files into the build directory.
+     *
+     * @see com.optum.gradle.tigergraph.GsqlCopySources
      */
     val COPY_SOURCES_TASK_NAME = "gsqlCopySources"
+
+    /**
+     * The name of the task that runs the interactive GSQL shell.
+     *
+     * @see com.optum.gradle.tigergraph.GsqlShell
+     */
+    val GSQL_SHELL_TASK_NAME = "gsqlShell"
 
     override fun apply(project: Project): Unit = project.run {
         // Register extension for dsl
@@ -24,7 +34,7 @@ open class GsqlPlugin : Plugin<Project> {
             create(COPY_SOURCES_TASK_NAME, GsqlCopySources::class.java)
         }
         project.tasks.run {
-            create("gsqlShell", GsqlShell::class.java)
+            create(GSQL_SHELL_TASK_NAME, GsqlShell::class.java)
         }
 
         with(project) {
