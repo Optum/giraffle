9fe1f54a765f173c85663162a5970d4c410c757c
Josh Meekhof <joshua_meekhof@optum.com>
2019-02-22 23:49:46 -0500

    Make GsqlCopySources use extension properties

--- src/main/kotlin/com/optum/gradle/tigergraph/GsqlCopySources.kt
+++ src/main/kotlin/com/optum/gradle/tigergraph/GsqlCopySources.kt
@@ -1,8 +1,10 @@
 package com.optum.gradle.tigergraph
 
-import org.apache.tools.ant.filters.ReplaceTokens
+// import org.apache.tools.ant.filters.ReplaceTokens
 import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.Input
+import org.gradle.api.file.DirectoryProperty
+// import org.gradle.api.provider.Property
+// import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
@@ -11,31 +13,26 @@ import java.io.File
 open class GsqlCopySources : DefaultTask() {
     private val extension: GsqlPluginExtension = project.extensions.findByName("tigergraph") as GsqlPluginExtension
 
-    @InputDirectory
-    val inputDir: File = project.file(extension.scriptDir)
+    @get:InputDirectory
+    val inputDir: DirectoryProperty = project.objects.directoryProperty()
 
-    @Input
-    val tokens: Map<String, String> = extension.tokens
+    /*
+    @get:Input
+    val tokens: Property<Map<String, String>> = extension.tokens
+    */
 
-    @OutputDirectory
+    @get:OutputDirectory
     val outputDir: File = project.file("${project.buildDir}/${extension.scriptDir}")
-
-    init {
-        group = "Development"
-        description = "Copy gsql script from scriptDir to build directory."
-    }
 
     @TaskAction
     fun copyFiles() {
-        inputDir.apply { parentFile.mkdirs() }
-
-        project.logger.lifecycle("tokens: ", tokens)
+        // project.logger.lifecycle("tokens: ", tokens)
 
         project.copy {
             it.run {
                 from(inputDir) {
                     include("**/*.gsql")
-                    filter(mapOf("tokens" to tokens), ReplaceTokens::class.java)
+                    // filter(mapOf("tokens" to tokens), ReplaceTokens::class.java)
                 }
                 into(outputDir)
             }
