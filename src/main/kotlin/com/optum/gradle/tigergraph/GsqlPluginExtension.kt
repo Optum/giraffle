6a107b8a611850fe60e71c84afdc9176413629d7
Josh Meekhof <joshua_meekhof@optum.com>
2019-02-22 23:40:44 -0500

    Make extensions properties, add javadocs

--- src/main/kotlin/com/optum/gradle/tigergraph/GsqlPluginExtension.kt
+++ src/main/kotlin/com/optum/gradle/tigergraph/GsqlPluginExtension.kt
@@ -5,11 +5,52 @@ import org.gradle.api.file.DirectoryProperty
 import org.gradle.api.provider.Property
 
 open class GsqlPluginExtension(project: Project) {
+    /**
+     * Returns the directory for gsql script sources.
+     *
+     * @return The input script directory
+     */
     val scriptDir: DirectoryProperty = project.objects.directoryProperty()
-    var tokens: Map<String, String> = emptyMap<String, String>()
-    var serverName: Property<String> = project.objects.property(String::class.java)
-    var userName: Property<String> = project.objects.property(String::class.java)
-    var password: Property<String> = project.objects.property(String::class.java)
-    var adminUserName: Property<String> = project.objects.property(String::class.java)
-    var adminPassword: Property<String> = project.objects.property(String::class.java)
+
+    /**
+     * Returns the tokens intended to be used in the Ant style filter.
+     *
+     * @return The map for token replacement
+     */
+    val tokens: Map<String, String> = emptyMap<String, String>()
+
+    /**
+     * Returns the server name for the tigergraph server.
+     *
+     * @return The tigergraph server
+     */
+    val serverName: Property<String> = project.objects.property(String::class.java)
+
+    /**
+     * Returns the non-privileged user to use for connecting to tigergraph
+     *
+     * @return The user for connecting to tigergraph
+     */
+    val userName: Property<String> = project.objects.property(String::class.java)
+
+    /**
+     * Returns the password associated with the non-privileged user
+     *
+     * @return The password for userName
+     */
+    val password: Property<String> = project.objects.property(String::class.java)
+
+    /**
+     * Returns the privileged user to use for connecting to tigergraph
+     *
+     * @return The user for connecting to tigergraph
+     */
+    val adminUserName: Property<String> = project.objects.property(String::class.java)
+
+    /**
+     * Returns the password associated with the non-privileged user
+     *
+     * @return The password for userName
+     */
+    val adminPassword: Property<String> = project.objects.property(String::class.java)
 }
