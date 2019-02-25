567c1bbab97237ffed36acd1eeb44a4aa133a8e6
Josh Meekhof <joshua_meekhof@optum.com>
2019-02-25 14:58:20 -0500

    Make configuration object vals const

--- src/main/kotlin/com/optum/gradle/tigergraph/Configurations.kt
+++ src/main/kotlin/com/optum/gradle/tigergraph/Configurations.kt
@@ -5,12 +5,12 @@ internal object Configurations {
      * The name of the runtime configuration for add Classpath dependencies to the plugin.
      *
      */
-    val gsqlRuntime = "gsqlRuntime"
+    const val gsqlRuntime = "gsqlRuntime"
 
     /**
      * The name of the extension for configuring the runtime behavior of the plugin.
      *
      * @see com.optum.gradle.tigergraph.GsqlPluginExtension
      */
-    val extensionName = "tigergraph"
+    const val extensionName = "tigergraph"
 }
