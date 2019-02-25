b78f7cf0de1ea2845d4bb0fbd1dc0e9b88a7065b
Josh Meekhof <joshua_meekhof@optum.com>
2019-02-25 14:56:42 -0500

    Add javadoc to gsqlRuntime val

--- src/main/kotlin/com/optum/gradle/tigergraph/Configurations.kt
+++ src/main/kotlin/com/optum/gradle/tigergraph/Configurations.kt
@@ -1,6 +1,10 @@
 package com.optum.gradle.tigergraph
 
 internal object Configurations {
+    /**
+     * The name of the runtime configuration for add Classpath dependencies to the plugin.
+     *
+     */
     val gsqlRuntime = "gsqlRuntime"
 
     /**
