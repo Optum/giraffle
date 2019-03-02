ef71d78eb1f30b5d4006e6622828bb86ceb17660
Josh Meekhof <joshua_meekhof@optum.com>
2019-03-02 11:39:53 -0500

    Use token MapProperty for ReplaceTokens

--- src/main/kotlin/com/optum/gradle/tigergraph/tasks/GsqlCopySources.kt
+++ src/main/kotlin/com/optum/gradle/tigergraph/tasks/GsqlCopySources.kt
@@ -1,9 +1,11 @@
 package com.optum.gradle.tigergraph.tasks
 
-// import org.apache.tools.ant.filters.ReplaceTokens
+import org.apache.tools.ant.filters.ReplaceTokens
 import com.optum.gradle.tigergraph.Configurations.scriptDirectoryName
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.provider.MapProperty
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputDirectory
 import org.gradle.api.tasks.OutputDirectory
 import org.gradle.api.tasks.TaskAction
@@ -16,10 +18,8 @@ open class GsqlCopySources : DefaultTask() {
     @get:InputDirectory
     val inputDir: DirectoryProperty = project.objects.directoryProperty()
 
-    /*
     @get:Input
-    val tokens: Property<Map<String, String>> = extension.tokens
-    */
+    val tokens: MapProperty<String, String> = project.objects.mapProperty(String::class.java, String::class.java)
 
     /**
      * The directory to use when outputting gsql sources.
@@ -31,11 +31,16 @@ open class GsqlCopySources : DefaultTask() {
     fun copyFiles() {
         outputDir.set(project.layout.buildDirectory.dir(scriptDirectoryName))
         // project.logger.lifecycle("tokens: ", tokens)
+        tokens.get().forEach { entry ->
+            logger.quiet("${entry.key}: ${entry.value}")
+        }
+
+        // inputs.properties(tokens)
         project.copy {
             it.run {
                 from(inputDir) {
                     include("**/*.gsql")
-                    // filter(mapOf("tokens" to tokens), ReplaceTokens::class.java)
+                    filter(mapOf("tokens" to tokens.get()), ReplaceTokens::class.java)
                 }
                 into(outputDir)
             }
