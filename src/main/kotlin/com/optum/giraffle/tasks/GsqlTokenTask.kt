eb95b1e49b2b71c13f31bf3cce69c14f5eed7b14
Josh Meekhof <joshua_meekhof@optum.com>
2019-06-24 14:33:08 -0400

    Activate task to obtain token from Tigergraph

--- src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
+++ src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
@@ -1,24 +1,19 @@
 package com.optum.giraffle.tasks
 
 import com.optum.giraffle.GsqlPluginExtension
-// import com.optum.giraffle.data.GsqlToken
 import khttp.get
 import org.gradle.api.DefaultTask
+import org.gradle.api.GradleException
 import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.Internal
-// import org.gradle.api.tasks.Nested
-// import org.gradle.api.tasks.TaskAction
+import org.gradle.api.tasks.TaskAction
 
 open class GsqlTokenTask() : DefaultTask() {
-
-    // TODO: Remove this type, add the token to the gsqlPluginExtension
-    // @Output
-    // val gsqlToken: GsqlToken = GsqlToken(project)
 
     @Internal
     private val gsqlPluginExtension: GsqlPluginExtension = project.extensions.getByType(GsqlPluginExtension::class.java)
 
-    @Input
+    @get:Input
     lateinit var secret: String
 
     private val host: String
@@ -26,17 +21,21 @@ open class GsqlTokenTask() : DefaultTask() {
 
     init {
         this.host = gsqlPluginExtension.serverName.get()
-        this.initToken()
+        // logger.info("Secret is ${this.secret}")
+        // this.initToken()
     }
 
+    @TaskAction
     fun initToken() {
+        logger.info("Secret is ${this.secret}")
         val url: String = "http://${this.host}:${this.defaultPort}/requesttoken"
         val r = get(url = url, params = mapOf("secret" to secret))
         logger.info("status code: {}", r.statusCode)
         logger.info("response: {}", r.jsonObject)
 
-        gsqlPluginExtension.token.set(r.jsonObject["code"].toString())
-
-        // gsqlToken.setCode(r.jsonObject["code"])
+        when (r.jsonObject["error"]) {
+            false -> gsqlPluginExtension.token.set(r.jsonObject["code"].toString())
+            true -> throw GradleException(r.jsonObject["message"].toString())
+        }
     }
 }
