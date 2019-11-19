3d3f2a64d2bbc6c8e5394ad8641d43ec68bb9c96
Josh Meekhof <joshua_meekhof@optum.com>
2019-11-19 11:55:58 +0530

    Add mavenlocal for gradle plugin source

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,13 @@
 
 rootProject.name = "giraffle"
 
+pluginManagement {
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("com.gradle.enterprise") version "3.0"
 }
