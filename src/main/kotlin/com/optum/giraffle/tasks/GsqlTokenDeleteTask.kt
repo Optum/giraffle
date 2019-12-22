359219ad80643e4d0610dc7233984098c0c39398
Josh Meekhof <jmeekhof@twotheleft.com>
2019-12-22 13:06:48 -0500

    DeleteToken uses extension properties

--- src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenDeleteTask.kt
+++ src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenDeleteTask.kt
@@ -1,18 +1,16 @@
 package com.optum.giraffle.tasks
 
 import khttp.delete
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
 open class GsqlTokenDeleteTask() : GsqlTokenAbstract() {
 
-    @get:Input
-    lateinit var token: String
-
     @TaskAction
     fun deleteToken() {
 
-        val r = delete(url = url(), params = mapOf("secret" to secret, "token" to token))
+        val r = delete(url = url(), params = mapOf(
+            "secret" to gsqlPluginExtension.authSecret.get(),
+            "token" to gsqlPluginExtension.token.get()))
         with(logger) {
             info("status code: {}", r.statusCode)
             info("response: {}", r.jsonObject)
