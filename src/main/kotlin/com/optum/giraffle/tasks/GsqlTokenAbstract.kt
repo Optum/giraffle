456767f8aecf4ba24678819eaff9935c43b3f6ee
Josh Meekhof <jmeekhof@twotheleft.com>
2019-12-22 12:17:31 -0500

    Make and abstract token task type
    Move logic for token url into an abstract class. This will enable use in
    token delete task as well.

---
+++ src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenAbstract.kt
@@ -0,0 +1,31 @@
+package com.optum.giraffle.tasks
+
+import com.optum.giraffle.GsqlPluginExtension
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+
+abstract class GsqlTokenAbstract() : DefaultTask() {
+    protected val gsqlPluginExtension: GsqlPluginExtension =
+        project.extensions.getByType(GsqlPluginExtension::class.java)
+
+    @get:Input
+    lateinit var secret: String
+
+    @Input
+    var useHttps: Boolean = false
+
+    protected val host: String
+    protected val defaultPort: String
+
+    init {
+        this.host = gsqlPluginExtension.serverName.get()
+        this.defaultPort = gsqlPluginExtension.restPort.get()
+    }
+
+    fun urlPrefix(): String = when (useHttps) {
+        true -> "https://"
+        false -> "http://"
+    }
+
+    fun url(): String = "${urlPrefix()}${this.host}:${this.defaultPort}/requesttoken"
+}
--- src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
+++ src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
@@ -1,32 +1,17 @@
 package com.optum.giraffle.tasks
 
-import com.optum.giraffle.GsqlPluginExtension
 import khttp.get
-import org.gradle.api.DefaultTask
 import org.gradle.api.GradleException
-import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.TaskAction
 
-open class GsqlTokenTask() : DefaultTask() {
-
-    private val gsqlPluginExtension: GsqlPluginExtension = project.extensions.getByType(GsqlPluginExtension::class.java)
-
-    @get:Input
-    lateinit var secret: String
-
-    private val host: String
-    private val defaultPort: String
-
-    init {
-        this.host = gsqlPluginExtension.serverName.get()
-        this.defaultPort = gsqlPluginExtension.restPort.get()
-    }
+open class GsqlTokenTask() : GsqlTokenAbstract() {
 
     @TaskAction
     fun initToken() {
-        val url: String = "http://${this.host}:${this.defaultPort}/requesttoken"
-        val r = get(url = url, params = mapOf("secret" to secret))
+
+        val r = get(url = url(), params = mapOf("secret" to secret))
         with(logger) {
+            // info("generated url: {}", r.url)
             info("status code: {}", r.statusCode)
             info("response: {}", r.jsonObject)
         }
