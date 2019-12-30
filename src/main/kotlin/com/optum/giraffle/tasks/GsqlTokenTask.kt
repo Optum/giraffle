e390060a1d75eb19c4a349d84ff547b9cd6d1f79
Josh Meekhof <joshua_meekhof@optum.com>
2019-12-30 13:52:41 -0500

    Adapted GsqlTokenTask to use moshi and okhttp

--- src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
+++ src/main/kotlin/com/optum/giraffle/tasks/GsqlTokenTask.kt
@@ -1,31 +1,24 @@
 package com.optum.giraffle.tasks
 
-import com.optum.giraffle.data.GToken
-import com.optum.giraffle.data.GsqlToken
-import com.squareup.moshi.Json
-import com.squareup.moshi.JsonClass
+import com.optum.giraffle.data.GsqlTokenSerializable
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import khttp.get
-import okhttp3.HttpUrl
-import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
+import java.io.IOException
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import org.gradle.api.GradleException
 import org.gradle.api.tasks.TaskAction
-import okhttp3.Request
-import org.json.JSONObject
-import java.io.IOException
 
-open class GsqlTokenTask() : GsqlTokenAbstract() {
+open class GsqlTokenTask : GsqlTokenAbstract() {
 
     private val client: OkHttpClient = OkHttpClient()
     private val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-    private val tokenMoshiAdapter = moshi.adapter(Token::class.java)
+    private val tokenMoshiAdapter = moshi.adapter(GsqlTokenSerializable::class.java)
 
     @TaskAction
     fun initToken() {
 
-        val x = httpUrl.newBuilder()
+        val x = getHttpUrl().newBuilder()
             .addQueryParameter("secret", gsqlPluginExtension.authSecret.get())
             .build()
 
@@ -40,54 +33,17 @@ open class GsqlTokenTask() : GsqlTokenAbstract() {
 
             val tok = tokenMoshiAdapter.fromJson(response.body!!.source())
 
-            // val jsonData = JSONObject(responseData)
-
             with(logger) {
                 info("token: {}", tok!!.token)
                 info("status code: {}", response.code)
-                info("message: {}", tok!!.message)
+                info("message: {}", tok.message)
                 info("response: {}", tok.toString())
             }
 
             when (tok!!.error) {
                 false -> gsqlPluginExtension.token.set(tok.token)
-                true -> throw tok.message?.let { GradleException(it) }!!
+                true -> throw GradleException(tok.message)
             }
-
-            /*
-            when (jsonData["error"]) {
-                false -> gsqlPluginExtension.token.set(jsonData["token"].toString())
-                true -> throw GradleException(jsonData["message"].toString())
-            }
-             */
         }
-
-        /*
-        val r = Request.Builder()
-            .url(url)
-
-        val r = get(url = url, params = mapOf(
-            "secret" to gsqlPluginExtension.authSecret.get()
-        ))
-        with(logger) {
-            // info("generated url: {}", r.url)
-            info("status code: {}", r.statusCode)
-            info("response: {}", r.jsonObject
-        }
-
-        when (r.jsonObject["error"]) {
-            false -> gsqlPluginExtension.token.set(r.jsonObject["token"].toString())
-            true -> throw GradleException(r.jsonObject["message"].toString())
-        }
-         */
     }
 }
-
-@JsonClass(generateAdapter = true)
-data class Token(
-    var code: String?,
-    var expiration: Int?,
-    var error: Boolean?,
-    var message: String?,
-    var token: String?
-)
