0a1ec96ecc99a61a26cf6e3db0d61dda4e7090b3
Josh Meekhof <jmeekhof@twotheleft.com>
2020-05-18 19:39:52 -0400

    Update versions for kotlin and okhttp libraries

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,11 +1,10 @@
 object Versions {
     const val junitPlatformVersion = "1.5.2"
-    const val kotlin = "1.3.60"
-    const val kotlinter = "2.2.0"
+    const val kotlin = "1.3.70"
+    const val kotlinter = "2.3.2"
     const val moshi = "1.9.2"
-    const val okhttp = "4.2.2"
+    const val okhttp = "4.7.1"
     const val pluginPublish = "0.10.1"
     const val saliman = "1.5.1"
-    const val site = "0.6"
     const val spek = "2.0.0"
 }
