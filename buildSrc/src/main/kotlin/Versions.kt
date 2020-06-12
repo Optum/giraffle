f49f0742d0d0063272764f891dcf54ad3a78fecb
Josh Meekhof <jmeekhof@twotheleft.com>
2020-06-12 19:29:31 -0400

    Update kotlinter version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
     const val junitPlatformVersion = "1.5.2"
     const val kotlin = "1.3.72"
-    const val kotlinter = "2.3.2"
+    const val kotlinter = "2.4.0"
     const val moshi = "1.9.2"
     const val okhttp = "4.7.1"
     const val pluginPublish = "0.12.0"
